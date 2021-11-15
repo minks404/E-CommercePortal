9a18d1f9b48a943653ee2448e1440c86917f12fa
minks404 <d3v.minks@gmail.com>
2021-11-15 22:01:50 +0530

    Update validate.php

--- validate.php
+++ validate.php
@@ -17,48 +17,17 @@
                 $update_query="UPDATE users SET validation = '1' WHERE user_ID = '$user_ID'";
                 $response = mysqli_query($con, $update_query);
 				
-				$subject = "Online Product Trading - User Validation Successfull";
-		        $message = "Congratulations! You have been successfully validated by our team. We welcome you aboard"
-							." and wish you all the best for your future endeavours on our platform!<\br> You can now"
-							."log in at our portal!";
-							
-				$query2="SELECT * FROM users WHERE user_ID = '$user_ID'";
-                $result = mysqli_query($con, $query2);
-							
-				$email = $result["email"]; 
-							
-				try {
-				
-				$mail->SMTPDebug = 2;									
-				$mail->isSMTP();											
-				$mail->Host	 = 'smtp.gmail.com';					
-				$mail->SMTPAuth = true;							
-				$mail->Username = 'd3v.minks@gmail.com';				
-				$mail->Password = '9899910158';						
-				$mail->SMTPSecure = 'tls';							
-				$mail->Port	 = 587;
-
-				$mail->setFrom('d3v.minks@gmail.com', 'Online Product Trading - Forgot Password');		
-				$mail->addAddress($email, 'User');
-				
-				$mail->isHTML(true);								
-				$mail->Subject = $subject;
-				$mail->Body = $message;
-				$mail->AltBody = $message;
-				
-			} catch (Exception $e) {
-				echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-			}
 
                 if(!$response){
                     
                     echo "<center><h1> User ID ".$user_ID." Validation Not Successfull!</h1></center>";
                     echo "<center><h3>".mysqli_error($con)."</h3></center>";
                 }
+				/*
 				else{
 					$mail->send();
 					echo "Mail has been sent successfully!";
 					echo " <h1> User Validation Successfull and Email Sent!.</h1>";
-				}
+				}*/
 
 ?>
